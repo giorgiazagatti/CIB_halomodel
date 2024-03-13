59b7a8abb6c6d4e86829448cdfcea41a55ecf158
giorgia.zagatti <giorgia.zagatti@gmail.com>
2024-03-13 10:53:01 +0000

    cleaned classes

--- cosmology.py
+++ cosmology.py
@@ -3,29 +3,16 @@ import numpy as np
 
 cosmo     = Planck18
 
+#set physical constants
+c  = 299792458
+
 class cosmo_param:
     def __init__(self, redshift, cosmological_param, cosmo):
         self.redshift           = redshift
         self.cosmological_param = cosmological_param
         self.cosmo              = cosmo
         
-        self.read_params()
         self.compute_params()
-
-    def read_params(self):
-        T_CMB = self.cosmological_param['T_CMB']
-        
-        par_tau          = 0.0544
-        par_ns           = 0.9649
-        par_As           = 2.101e-9
-        par_pivot_scalar = 0.05
-
-        self.par_tau          = par_tau 
-        self.par_ns           = par_ns
-        self.par_As           = par_As
-        self.par_pivot_scalar = par_pivot_scalar
-
-        return T_CMB
 
     def compute_params(self):
         cosmo = self.cosmo
@@ -40,7 +27,7 @@ class cosmo_param:
         par_omega_b = om_b * h ** 2
 
         D_a   = cosmo.angular_diameter_distance(self.redshift).value
-        D_h   = self.cosmological_param['c'] / H_0
+        D_h   = c / H_0
         dV_dz = D_h * ((1 + self.redshift) ** 2 * D_a ** 2) / np.sqrt(om_l + (1 + self.redshift) ** 3 * om_m) / h ** -3
 
         self.om_b = om_b
@@ -51,6 +38,7 @@ class cosmo_param:
     def read_matter_PS(self):
         user_matterPS = self.cosmological_param['user_matterPS']
         if user_matterPS is None:
+            print('Using pre-computed linear matter power spectrum with CAMB')
             matter_PS = np.loadtxt(self.cosmological_param['matter_PS'])
             k_array_T = matter_PS[:,0]
             k_array   = np.transpose(k_array_T)
@@ -58,6 +46,7 @@ class cosmo_param:
             Pk_array_T = matter_PS[:,1:]
             Pk_array   = np.transpose(Pk_array_T)
         else:
+            print('Using external linear matter power spectrum provided by the user')
             k_array, Pk_array = np.loadtxt(user_matterPS)
 
         return k_array, Pk_array
--- matterPS.py
+++ matterPS.py
@@ -3,33 +3,25 @@ import camb
 from camb import model, initialpower
 
 class matter_PS:
-    #def __init__(self, par_h, par_omega_b, par_omega_m, par_tau, par_ns, par_As, par_pivot_scalar, redshift, l2_max, lenLs, dl_CMB):
-    def __init__(self, redshift, h, cosmo_param):
-        #self.par_h            = par_h
-        #self.par_omega_b      = par_omega_b
-        #self.par_omega_m      = par_omega_m
-        #self.par_tau          = par_tau
-        #self.par_ns           = par_ns
-        #self.par_As           = par_As
-        #self.par_pivot_scalar = par_pivot_scalar
-        self.redshift         = redshift
-        self.h                = h
-        #self.l2_max           = l2_max
-        self.cosmo_param = cosmo_param
+    def __init__(self, redshift, h, cosmo_param, cosmological_param):
+        self.redshift           = redshift
+        self.h                  = h
+        self.cosmo_param        = cosmo_param
+        self.cosmological_param = cosmological_param
 
     #computation of the lin matter PS
     def lin_matter_PS(self):
+        print('Start the computation of the linear matter power spectrum')
         par_h = self.h * 100
         par_omega_b = self.cosmo_param.om_b * self.h ** 2
         par_omega_m = self.cosmo_param.om_m * self.h ** 2
 
         pars = camb.CAMBparams()
         pars.set_cosmology(
-            H0=par_h, ombh2=par_omega_b, omch2=par_omega_m - par_omega_b, tau=self.cosmo_param.par_tau
+            H0=par_h, ombh2=par_omega_b, omch2=par_omega_m - par_omega_b, tau=self.cosmological_param['tau']
         )
-        pars.InitPower.set_params(ns=self.cosmo_param.par_ns, As=self.cosmo_param.par_As, pivot_scalar=self.cosmo_param.par_pivot_scalar)
+        pars.InitPower.set_params(ns=self.cosmological_param['ns'], As=self.cosmological_param['As'], pivot_scalar=self.cosmological_param['pivot_scalar'])
         pars.set_matter_power(redshifts=self.redshift, kmax=1e2)
-        #pars.set_for_lmax(self.l2_max, lens_potential_accuracy=0)
 
         pars.NonLinear = model.NonLinear_none
         results        = camb.get_results(pars)
